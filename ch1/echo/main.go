8fa9a5bc9e4dd4e4596ef97671e1417950708e89
Hao Nguyen <hao@haonguyen.tech>
2024-08-23 15:38:10 +0700

    chore: add todo for later exercise with echo

--- ch1/echo/main.go
+++ ch1/echo/main.go
@@ -4,13 +4,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
+	// TODO:Exercise 1.3:
+	// Experiment to measure the difference in running time between our
+	// potentially inefficient versions and the one that uses strings.Join.
+	// (Section 1.6 illustrates part of the time package,
+	// and Section 11.4 shows how to write benchmark tests
+	// for systematic performance evaluation.)
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
