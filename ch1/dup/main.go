18a66f722af7f40a5a041909c2ef5c7396f4494f
Hao Nguyen <hao@haonguyen.tech>
2024-08-23 16:13:53 +0700

    feat: add dup1

---
+++ ch1/dup/main.go
@@ -0,0 +1,23 @@
+// print duplicate and the count in standard in
+// Run by start the command, input some duplicate value with new line, press ctrl+D to end input and see the output
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
